3fdfd5d7bccf1fed6fb3da5607ce711c5bf6e38b
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-04 20:33:06 +0000

    added code

--- js/script.js
+++ js/script.js
@@ -2,8 +2,19 @@
 //
 // Created by: Kaitlyn Ip
 // Created on: Mar 2022
-// This file contains xxx
+// This file contains the JS functions for index.html
 
+'use strict'
 /**
- * This function xxx.
+ * This function calculates volume of pyramid.
  */
+function calculate () {
+  // input
+  const radius = parseFloat(document.getElementById("radius-of-sphere").value)
+
+  // process
+  const volume = 4 / 3 * Math.PI * radius ** 3
+
+  // output
+  document.getElementById('volume').innerHTML = 'volume is: ' + volume.toFixed(2) + ' mm³'
+}
